b3d30dea64e7245380c1f071dcc03e906ff002e9
01399619 <chenying63@sfmail.sf-express.com>
2023-01-03 19:29:19 +0800

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import Counter from './components/Counter';
+import * as React from 'react';
+import BasicLayout from '@/pages/BasicLayout';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from '@/store';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Error from "@/pages/Error";
+import Home from "@/pages/Home";
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-      <Counter value={4} />
-    </div>
+    <Provider store={store}>
+      <HashRouter>
+          <BasicLayout>
+            <Routes>
+                <Route path="/error" element={<Error />}></Route>
+                <Route path="/" element={<Home />}></Route>
+                {/* {Routes} */}
+            </Routes>
+            </BasicLayout>
+      </HashRouter>
+    </Provider>
   );
 }
 
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
-import { NAME } from '../utils/const'
+import { NAME } from '@/utils/const';
 
-// 创建类型接口
-export interface Iprops {
-    value: number
+interface Iprops {
+    value: number;
+    onIncrement?: () => void;
+    onDecrement?: () => void;
 }
 
-// 使用接口代替propTypes进行类型校验
-const Counter = ({ value }: Iprops) => {
-    return <p> {NAME} click {value} times</p>
+const Counter: React.FC<Iprops> = ({value, onIncrement, onDecrement}: Iprops) => {
+    return (
+        <div>
+            <p> {NAME} click {value} times</p>
+            <button onClick={ onIncrement } style={{ marginRight: 20 }}> +  </button>
+            <button onClick={ onDecrement }> - </button>
+        </div>
+    )
 }
 
 
